6e27eca456374fa8274c63eada89200db7bef503
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-01-19 05:54:24 +0100

    feat: data-type lectures projects

---
+++ projects/data-types/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, {} {}!", "Anu", "Adebayo");
+
+    let mut age = 33;
+    let birth_year = 1991;
+
+    println!("I am {} years old", age);
+
+    let birth_year = birth_year - 1;
+
+    age = 25;
+
+    println!("I am now {} years old", age);
+    println!("I was born in {}", birth_year)
+}
